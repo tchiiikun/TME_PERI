c8f14125058969237e2356dcda848ed22d6c37b4
William Fabre [m1] <fabre@josquin.soc.lip6.fr>
2018-05-24 18:41:30 +0200

    supression

--- TP_07/projet_arduino/server-fake/server/create_db.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import sqlite3
-sqlite_file='db.sqlite'
-conn = sqlite3.connect(sqlite_file)
-c = conn.cursor()
-conn.execute("CREATE TABLE measures (timestamp DATETIME, measure INTEGER)")
-conn.commit()
-conn.close()
